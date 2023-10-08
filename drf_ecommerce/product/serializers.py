03b0a2dbe8e38293d9b7de4069aaf6b1d828f209
hamidhagh <hamidhaghverdi2002@gmail.com>
2023-10-08 10:00:53 +0200

    nc dev-1.0.4

--- drf_ecommerce/product/serializers.py
+++ drf_ecommerce/product/serializers.py
@@ -1,10 +1,9 @@
 from rest_framework import serializers
 
-from .models import Brand, Category, Product, ProductLine, ProductImage
+from .models import Attribute, AttributeValue, Brand, Category, Product, ProductImage, ProductLine
 
 
 class CategorySerializer(serializers.ModelSerializer):
-
     category_name = serializers.CharField(source="name")
 
     class Meta:
@@ -24,10 +23,26 @@ class ProductImageSerializer(serializers.ModelSerializer):
         exclude = ("id", "productline")
 
 
+class AttributeSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Attribute
+        fields = ("name", "id")
+
+
+class AttributeValueSerializer(serializers.ModelSerializer):
+    attribute = AttributeSerializer(many=False)
+
+    class Meta:
+        model = AttributeValue
+        fields = (
+            "attribute",
+            "attribute_value",
+        )
+
 
 class ProductLineSerializer(serializers.ModelSerializer):
-    product_image = ProductImageSerializer(many=True)
- 
+    attribute_value = AttributeValueSerializer(many=True)
+
     class Meta:
         model = ProductLine
         fields = (
@@ -36,14 +51,26 @@ class ProductLineSerializer(serializers.ModelSerializer):
             "stock_qty",
             "order",
             "product_image",
+            "attribute_value",
         )
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        av_data = data.pop("attribute_value")
+        attr_values = {}
+        for key in av_data:
+            attr_values.update({key["attribute"]["id"]: key["attribute_value"]})
+        data.update({"specification": attr_values})
+
+        return data
 
 
 class ProductSerializer(serializers.ModelSerializer):
     brand_name = serializers.CharField(source="brand.name")
     category_name = serializers.CharField(source="category.name")
     product_line = ProductLineSerializer(many=True)
-    
+    attribute = serializers.SerializerMethodField()
+
     class Meta:
         model = Product
         fields = (
@@ -53,6 +80,19 @@ class ProductSerializer(serializers.ModelSerializer):
             "brand_name",
             "category_name",
             "product_line",
+            "attribute",
         )
 
+    def get_attribute(self, obj):
+        attribute = Attribute.objects.filter(product_type_attribute__product__id=obj.id)
+        return AttributeSerializer(attribute, many=True).data
 
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        av_data = data.pop("attribute")
+        attr_values = {}
+        for key in av_data:
+            attr_values.update({key["id"]: key["name"]})
+        data.update({"type specification": attr_values})
+
+        return data
